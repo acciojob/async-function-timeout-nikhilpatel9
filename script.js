052987376388023eebe43ae5e7248f79ef0b7f86
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2026-05-30 21:44:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+const time = document.getElementById("delay");
+const text = document.getElementById("text");
+const btn = document.getElementById("btn");
+const outPut = document.getElementById("output");
+
+const handleSubmit = async function() {
+    const delayValue = Number(time.value);
+    const textValue = text.value;
+
+    const delayPromise = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(textValue);
+        }, delayValue*1000); 
+    });
+
+    const data = await delayPromise;
+    outPut.innerHTML = `<p>${data}</p>`;
+};
+
+btn.addEventListener('click', handleSubmit);
